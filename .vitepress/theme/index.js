cafdf7191ea9e896e643bf8e2a8a2308498def19
ah-rasel5 <127735115+ah-rasel5@users.noreply.github.com>
2026-01-23 11:56:12 +0600

    Feedback Added

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -2,10 +2,14 @@ import DefaultTheme from 'vitepress/theme';
 import './custom.css';
 // import ZoomableImage from './components/ZoomableImage.vue';
 import { generateSchema } from './schema.js';
+import Feedback from './components/Feedback.vue' // Import the file for feedback widget
+import Layout from './Layout.vue' // Import new layout file
+
 
 export default {
   extends: DefaultTheme,
-  enhanceAppWithTabs({ app }) {
-    // Add global SEO enhancements
+  Layout,
+  enhanceApp({ app }) {
+    app.component('Feedback', Feedback)
   }
-};
+} 
